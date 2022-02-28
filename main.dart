62ec207dc8cd1a484a805899a45727b7d8b09800
Mohammed Mahmoud Ali AbdAllah <96302371+Jpeilx@users.noreply.github.com>
2022-02-28 23:53:59 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+void main (){
+  print('welcome to Turkish Airline And Turkish five star Hotel ');
+  print('what do you want ?? \nBook room or a Flight!');
+  var s = stdin.readLineSync();
+
+
+
+
+
+
+}
